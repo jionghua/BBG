94d1c946c7061e32d7cf7a5eca028b7a07e5b5c7
Jionghua Wang <dogyearjoanna@jionghua-2.local>
2015-06-18 12:54:10 -0700

    Initial Commit

---
+++ BurgerGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BurgerGame
+//
+//  Created by Jionghua Wang on 6/18/15.
+//  Copyright (c) 2015 JWFY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BurgerGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  BurgerGame
+//
+
+//  Copyright (c) 2015 JWFY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
